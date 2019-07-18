550d2f445e4d54276ead710c41066ec82dc3d3da
Martha Plantz <maplantz89@gmail.com>
2019-07-18 13:36:11 -0700

    added a couple more tests

--- test/app.test.js
+++ test/app.test.js
@@ -52,13 +52,61 @@ describe('App', ()=> {
       });
     });
 
-    
+    context('incorrect information', () => {
+      it('should return 400 with message with incorrect username', () => {
+        return supertest(app)
+          .post('/register')
+          .send({
+            'username': 'No',
+            'password': 'c00d1ng1sc00l',
+            'favoriteClub': 'Cache Valley Stone Society',
+            'newsLetter': 'true'
+          })
+          .expect(400, 'Username number be between 6 and 20 characters long');
+      });
+      it('should return 400 with message with incorrect password', () => {
+        return supertest(app)
+          .post('/register')
+          .send({
+            'username': 'Martha',
+            'password': 'NO',
+            'favoriteClub': 'Cache Valley Stone Society',
+            'newsLetter': 'true'
+          })
+          .expect(400, 'Password number be between 8 and 36 characters long');
+      });
+      //not sure how to make this work for the last validation statement
+      it('should return 400 if password does not have a number', () => {
+        return supertest(app)
+          .post('/register')
+          .send({
+            'username': 'Martha',
+            'password': 'NO',
+            'favoriteClub': 'Cache Valley Stone Society',
+            'newsLetter': 'true'
+          })
+          .expect(400, 'Password must contain at least one digit');
+      });
+    });
+
   });
 
   context('Delete request', () => {
-    it('should delete based on userId');
-  });
+    // //how do you mock deleting a user
 
-  context('errorHanlder', () => {
+    // it('should delete based on userId', () => {
+    //   return supertest(app)
+    //     .delete('/user/:userId')
+    //     .expect(204);
+
+    // });
+
+    //
+    // it('should return 404 if user is not found', ()=> {
+    //   return supertest(app)
+    //     .delete('user/:userId')
+    //     .send({})
+    //     .expect(404, 'User not found');
+    // });
   });
 });
